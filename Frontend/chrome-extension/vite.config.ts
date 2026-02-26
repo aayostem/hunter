a1a35da0aa2b517e22fac3fc378caa4d0afa1aae
janto-pee <ayobami_adejumo@yahoo.com>
2026-02-26 18:27:26 +0100

    enabling full backend deployment

--- Frontend/chrome-extension/vite.config.ts
+++ Frontend/chrome-extension/vite.config.ts
@@ -1,23 +1,59 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        popup: resolve(__dirname, "src/popup/index.html"),
-        background: resolve(__dirname, "src/background/service-worker.ts"),
-        content: resolve(__dirname, "src/content-scripts/gmail-injector.ts"),
+        background: resolve(__dirname, 'src/background/service-worker.ts'),
+        popup: resolve(__dirname, 'src/popup/index.html'),
+        'content-scripts/gmail-injector': resolve(__dirname, 'src/content-scripts/gmail-injector.ts'),
       },
       output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
+        entryFileNames: (chunk) => {
+          if (chunk.name === 'background') return 'background.js';
+          if (chunk.name === 'content-scripts/gmail-injector') return 'content-scripts/gmail-injector.js';
+          return '[name].js';
+        },
+        chunkFileNames: 'chunks/[name].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'inject.css') return 'styles/inject.css';
+          return 'assets/[name].[ext]';
+        },
       },
     },
   },
 });
+
+
+// FIREFOX
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import { resolve } from "path";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   build: {
+//     outDir: "dist",
+//     rollupOptions: {
+//       input: {
+//         background: resolve(__dirname, "src/background.ts"),
+//         popup: resolve(__dirname, "src/popup/index.html"), // inside src/popup
+//         "content-scripts/gmail-injector": resolve(
+//           __dirname,
+//           "src/content-scripts/gmail-injector.ts"
+//         ),
+//       },
+//       output: {
+//         entryFileNames: "[name].js",
+//         chunkFileNames: "[name].js",
+//         assetFileNames: "[name].[ext]",
+//       },
+//     },
+//   },
+// });
