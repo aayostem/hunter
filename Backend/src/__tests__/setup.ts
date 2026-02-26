abe56dc5164389cea754a8e4255b32a063b274ed
janto-pee <ayobami_adejumo@yahoo.com>
2026-02-26 12:30:42 +0100

    adding test

--- Backend/src/__tests__/setup.ts
+++ Backend/src/__tests__/setup.ts
@@ -4,3 +4,9 @@ import { prisma } from '../lib/prisma';
 afterAll(async () => {
   await prisma.$disconnect();
 });
+
+
+// Add a dummy test to satisfy Jest
+test('setup file', () => {
+  expect(true).toBe(true);
+});
